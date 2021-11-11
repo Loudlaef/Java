486f085ce8ef601e27a5d2b3d9ae06b68eb356b9
loudlaef <cho.byungkyu@gmail.com>
2021-11-11 10:18:24 +0900

    Initial Create

---
+++ HelloWithVSCode/HelloWithVSCode.java
@@ -0,0 +1,16 @@
+/**
+ * HelloWithVSCode
+ */
+public class HelloWithVSCode {
+
+    public static void main(String[] args) {
+          String str1 = "Hello World";
+          String str2 = "Java";
+          String str3 = "Nice to meet you!";
+          String str4 = "한글 바이!" ;
+          System.out.println(str1);
+          System.out.println(str2);
+          System.out.println(str3);
+          System.out.println(str4);
+    }
+}
